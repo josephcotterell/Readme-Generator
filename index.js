e3791b5b4f250b84dc40ae023a2ebc403eea301d
joseph <josephcotterell@hotmail.co.uk>
2023-01-15 11:26:26 +0000

    completed list of questions

--- index.js
+++ index.js
@@ -1,27 +1,71 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const { default: Choices } = require("inquirer/lib/objects/choices");
 const { type } = require("os");
 const path = require("path");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    
-{
- type: "input",
-name: 'title',
-message:'enter your project',
+  {
+    type: "input",
+    name: "title",
+    message: "enter your project",
+  },
 
-}
+  {
+    type: "input",
+    name: "description",
+    message: "please provide the purpose of this project",
+  },
 
-{
-type: 'input',
-name: 'description',
-message: 'please provide the purpose of this project',
+  {
+    type: "checkbox",
+    name: "license",
+    message: "please select a license for this project",
+    Choices: ["boost1.0", "MIT", "MPL2.0", "APATCHE2.0", "none"],
+  },
 
+  {
+    type: "input",
+    name: "require",
+    message: "list any project dependancies here",
+  },
 
-}
+  {
+    type: "input",
+    name: "usage",
+    message: " please list the names of languages for this project",
+  },
 
+  {
+    type: "input",
+    name: "creator",
+    message: "state your Github username",
+  },
 
+  {
+    type: "input",
+    name: "name",
+    message: "state your full name",
+  },
+
+  {
+    type: "input",
+    name: "email",
+    message: "please provide a valid email address",
+  },
+
+  {
+    type: "input",
+    name: "contributors",
+    message: "please provide a list of contributors",
+  },
+
+  {
+    type: "input",
+    name: "test",
+    message: "please provide a walkthrough of required tests if applicable",
+  },
 ];
 
 // TODO: Create a function to write README file
