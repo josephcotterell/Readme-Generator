798f5b0b90ac5481ff15172affa288b030c71145
joseph <josephcotterell@hotmail.co.uk>
2023-01-15 11:04:35 +0000

    added some arrays

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const { type } = require("os");
+const path = require("path");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+{
+ type: "input",
+name: 'title',
+message:'enter your project',
+
+}
+
+{
+type: 'input',
+name: 'description',
+message: 'please provide the purpose of this project',
+
+
+}
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
